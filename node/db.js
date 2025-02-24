b0a12e79ae46c766661b7218bdcee4bbe1b2d0d6
chmpagnegaby <hcgabriel7@gmail.com>
2025-02-24 18:04:32 +0100

    BD metida

--- node/db.js
+++ node/db.js
@@ -1,13 +1,12 @@
-import pgPromise from "pg-promise"
+import pgPromise from "pg-promise";
+require('dotenv').config();  // Carga las variables de entorno de .env
 
-//Configuro la base de datos
+// Usamos la URL desde el archivo .env
 const db = pgPromise()({
-    host: process.env.NODE_ENV === 'production' ? 'db' : 'localhost',  // Cambia el host según el entorno
-    port: 5432,
-    database: 'skyjo_entrega',
-    user: 'root',
-    password: 'root',
+  connectionString: process.env.DATABASE_URL,  // Carga la URL de conexión desde las variables de entorno
+  ssl: {
+    rejectUnauthorized: false  // Asegura que la conexión sea segura con SSL
+  }
 });
 
 export default db;
-
